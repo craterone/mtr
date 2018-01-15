aa6534c4d06942803df7e61e074dccf72ab45fd7
caelums <ccaelums@gmail.com>
2018-01-15 17:33:10 +0800

    add name and test id

--- src/mtr.cr
+++ src/mtr.cr
@@ -24,20 +24,40 @@ module Mtr
       flag.description = "The server for storing the report."
     end
 
+    cmd.flags.add do |flag|
+      flag.name = "name"
+      flag.short = "-n"
+      flag.long = "--name"
+      flag.default = "abc"
+      flag.description = "the name of the tester "
+    end
+
     cmd.run do |options, arguments|
       address = options.string["address"]
       server = options.string["server"]
+      name = options.string["name"]
 
       puts "start testing..."
 
       output = IO::Memory.new
       Process.run("mtr", ["-jn", "baidu.com"], output: output)
-      result = output.to_s
+
+      tmp_result = JSON.parse(output.to_s)
+      # puts house.to_json
       output.close
 
+      test_id = Time.new.epoch_ms
+      result = JSON.build do |json|
+        json.object do
+          json.field "name", name
+          json.field "id", test_id
+          json.field "data", tmp_result
+        end
+      end
       # puts result
       HTTP::Client.post(server, headers: HTTP::Headers{"Content-Type" => "application/json"}, body: result) do |response|
         puts response.body_io.gets # => "..."
+        puts "test id : #{test_id}"
       end
     end
   end
